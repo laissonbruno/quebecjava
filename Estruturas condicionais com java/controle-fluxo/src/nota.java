7397012adedf5d94af08d90e1599f2154a162d9f
Laisson Bruno <laissontkd@hotmail.com>
2022-09-26 19:20:14 -0300

    codigos iniciados

---
+++ Estruturas condicionais com java/controle-fluxo/src/nota.java
@@ -0,0 +1,14 @@
+public class nota {
+    public static void main(String[] args) {
+        int nota = 10;
+
+        if (nota >= 7)
+            System.out.println("Aprovado");
+
+        else if (nota >= 5 && nota < 7)
+            System.out.println("Recuperação");
+
+        else
+            System.out.println("Reprovado");
+    }
+}
